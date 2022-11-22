219c7007e94cbbc1ec837ce3dba25857a87a0a4b
Michalowski <michaloa@nih.gov>
2022-11-21 20:05:44 -0500

    update source integration code

--- Code/integration_analysis/00_SourceIntegrationSEEP.R
+++ Code/integration_analysis/00_SourceIntegrationSEEP.R
@@ -1,4 +1,3 @@
-rm(list = ls())
 setwd("C://PROJECTS/P2022/SEEP_Manuscript")
 
 #cat(sprintf("source('%s', echo=TRUE)\n", list.files("Codes", full=T, rec=T)[-1]))
@@ -8,42 +7,42 @@ setwd("C://PROJECTS/P2022/SEEP_Manuscript")
 # Combine separate SEEP (no integration)
 
 # Integrate SEEP
-source('Codes/01A_SelectSEEP.R', echo = TRUE)    #OK
-source('Codes/01B_IntegrateSEEP.R', echo = TRUE) #OK
-source('Codes/01C_DaSEEP.R', echo = TRUE)        #OK
-source('Codes/01D_MarkersSEEP.R', echo = TRUE)   #OK
-source('Codes/01E_OraSEEP.R', echo = TRUE)       #OK
+source('Codes/01A_SelectSEEP.R', echo = TRUE)
+source('Codes/01B_IntegrateSEEP.R', echo = TRUE)
+source('Codes/01C_DaSEEP.R', echo = TRUE)
+source('Codes/01D_MarkersSEEP.R', echo = TRUE)
+source('Codes/01E_OraSEEP.R', echo = TRUE)
 
-source('Codes/01G_SeparateSEEP.R', echo = TRUE)  #OK
+source('Codes/01G_SeparateSEEP.R', echo = TRUE)
 
 # Transfer SEEP to SS2
-source('Codes/02A_ProcessSS2.R', echo = TRUE)    #OK
-source('Codes/02B_TransferSS2.R', echo = TRUE)   #OK
+source('Codes/02A_ProcessSS2.R', echo = TRUE)
+source('Codes/02B_TransferSS2.R', echo = TRUE)
 
 # Transfer SEEP to 10X
-source('Codes/03A_process10X.R', echo = TRUE)    #OK
-source('Codes/03B_Transfer10X.R', echo = TRUE)   #OK
+source('Codes/03A_process10X.R', echo = TRUE)
+source('Codes/03B_Transfer10X.R', echo = TRUE)
 
 # Calculate pathway scores
-source('Codes/01F_OraScoresSEEP.R', echo = TRUE) #OK
-source('Codes/02C_OraScoresSS2.R', echo = TRUE)  #OK
-source('Codes/03C_OraScores10X.R', echo = TRUE)  #OK
+source('Codes/01F_OraScoresSEEP.R', echo = TRUE)
+source('Codes/02C_OraScoresSS2.R', echo = TRUE)
+source('Codes/03C_OraScores10X.R', echo = TRUE)
 
 # Manuscript Figures ====
 
 ## fig 6
-source('Codes/Fig6ABC.R', echo = TRUE)  # OK
-source('Codes/Fig6E.R', echo=TRUE)      # OK
-source('Codes/Fig6D.R', echo=TRUE)      # OK
+source('Codes/Fig6ABC.R', echo = TRUE)
+source('Codes/Fig6E.R', echo=TRUE)
+source('Codes/Fig6D.R', echo=TRUE)
 
 ## fig 7
-source('Codes/Fig7ABC.R', echo=TRUE) # OK
-source('Codes/Fig7DEF.R', echo=TRUE) # OK
-source('Codes/Fig7GHI.R', echo=TRUE) # OK
+source('Codes/Fig7ABC.R', echo=TRUE)
+source('Codes/Fig7DEF.R', echo=TRUE)
+source('Codes/Fig7GHI.R', echo=TRUE)
 
 # Supplement Figures ====
-source('Codes/SFigXAD.R', echo=TRUE) # OK
-source('Codes/SFigXE.R', echo=TRUE)  # OK
+source('Codes/SFigXAD.R', echo=TRUE)
+source('Codes/SFigXE.R', echo=TRUE)
 
 
 
